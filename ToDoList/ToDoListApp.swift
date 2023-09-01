ce441c0ab2d4d53ec7f16acfe6598a8c7b92ffb4
Paulo <paulo.adeva@gmail.com>
2023-09-02 00:02:50 +1000

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Paulo Adeva on 2/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
